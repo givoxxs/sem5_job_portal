f8ddb21f86a41039a111f24f0081b6ac474de0b0
givoxxs <toanyogame@gmail.com>
2024-12-16 15:45:37 +0700

    fix classpath

--- src/main/java/model/bo/JobBO.java
+++ src/main/java/model/bo/JobBO.java
@@ -1,5 +1,6 @@
 package model.bo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.bean.Job;
@@ -7,10 +8,10 @@ import model.dao.JobDAO;
 
 public class JobBO {
 	// instance
-	private static JobBO instance;
-	private JobBO() {
+	public static JobBO instance;
+	public JobDAO jobDAO = JobDAO.getInstance();
+	public JobBO() {}
 
-	}
 	public static JobBO getInstance() {
 		if (instance == null) {
 			instance = new JobBO();
@@ -20,62 +21,37 @@ public class JobBO {
 	
 	//select all jobs available
 	public List<Job> getAllAvailableJobs() {
-		return JobDAO.getInstance().getAllAvailableJobs();
+		return jobDAO.getAllAvailableJobs();
 	}
 	
-	//select all job
-	public List<Job> getAllJobs() {
-		return JobDAO.getInstance().getAllJobs();
+	public Job getJobById(String jobId) {
+		return jobDAO.getJobById(jobId);
 	}
 	
-	//select job by employer id
-	public List<Job> getJobByEmployerId(String id) {
-		return JobDAO.getInstance().getJobsByEmployerId(id);
+	public List<Job> getTopLatestJobs(int limit) {
+		return jobDAO.getTopLatestJobs(limit);
 	}
 	
-	//select job by id
-	public Job getJobById(String id) {
-		return JobDAO.getInstance().getJobById(id);
+	public List<Job> getRandomJobs(int num) {
+		return jobDAO.getRandomJobs(num);
 	}
 	
-	//search job by name
-	public List<Job> searchJobsByName(String name) {
-		return JobDAO.getInstance().searchJobsByName(name);
+	public List<String> getDistinctLocations() {
+	    // Khởi tạo danh sách các địa điểm cứng định
+	    ArrayList<String> locations = new ArrayList<String>();
+	    locations.add("Hà Nội");
+	    locations.add("Hồ Chí Minh");
+	    locations.add("Đà Nẵng");
+	    locations.add("Khác");
+
+	    return locations;
 	}
-	
-	//add job
-	public boolean addJob(String employerId, String title, String description, String salaryRangeId, String location, String jobType, String experience) {
-		return JobDAO.getInstance().addJob(employerId, title, description, salaryRangeId, location, jobType, experience);
-	}
-	
-	//update job
-	public boolean updateJob(String id, String title, String description, String salaryRangeId, String location,
-			String jobType, String experience) {
-		return JobDAO.getInstance().updateJob(id, title, description, salaryRangeId, location, jobType, experience);
-	}
-	
-	//Update available job
-	public boolean updateAvailableJob(String id, String status) {
-		boolean result;
-		if (status.equals("true")) {
-			result = true;
-		} else {
-			result = false;
-		}
-		return JobDAO.getInstance().updateJobAvailable(id, result);
-	}
-	
-	//search job
-	public List<Job> searchJobs(String jobName, String salaryRangeId, String jobType, String experience,
-			String location, int page) {
-		return JobDAO.getInstance().searchJobs(jobName, salaryRangeId, jobType, experience, location, page);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+    public List<Job> searchJobs(String jobName, String salaryRangeId, String jobType, String experience, String location, int page) {
+        return jobDAO.searchJobs(jobName, salaryRangeId, jobType, experience, location, page);
+    }
+
+    public int getTotalPages(String jobName, String salaryRangeId, String jobType, String experience, String location) {
+        return jobDAO.getTotalPages(jobName, salaryRangeId, jobType, experience, location);
+    }
 }
--- src/main/java/utils/DBConnect.java
+++ src/main/java/utils/DBConnect.java
@@ -38,8 +38,12 @@ public class DBConnect {
 				Class.forName("com.mysql.jdbc.Driver");
 				connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			} catch (ClassNotFoundException e) {
-				throw new SQLException("JDBC Driver not found", e);
-			}
+				System.err.println("JDBC Driver not found: " + e.getMessage());
+	            throw new SQLException("JDBC Driver not found", e);
+			} catch (SQLException e) {
+	            System.err.println("Failed to connect to the database: " + e.getMessage());
+	            throw e;
+	        }
 		}
 		
 		return connection;
